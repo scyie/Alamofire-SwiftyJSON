20a17af1e402962a07d5a155fa6b179b52584bcb
scyie <47838491+scyie@users.noreply.github.com>
2019-10-24 14:54:09 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,19 +3,19 @@ import PackageDescription
 
 let package = Package(
     name: "Alamofire-SwiftyJSON",
+    products: [
+        .library(name: "Alamofire-SwiftyJSON", targets: ["Alamofire-SwiftyJSON"])
+    ],
     dependencies: [
         .package(url: "https://github.com/Alamofire/Alamofire.git", from: "4.9.0"),
-        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "4.0.0")
-    ],
-    products: [
-        .library(
-            name: "Alamofire-SwiftyJSON",
-            targets: ["Alamofire-SwiftyJSON"])
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "4.0.0"),
     ],
     targets: [
         .target(
             name: "Alamofire-SwiftyJSON",
-            dependencies: ["Alamofire", "SwiftyJSON"],
-            path: "Source")
+            dependencies: [
+                "Alamofire", 
+                "SwiftyJSON"
+            ])
     ]
 )
