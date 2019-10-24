2b57d3735a4ae2e68c66d838cfff485f5ec6fe70
scyie <47838491+scyie@users.noreply.github.com>
2019-10-24 12:40:47 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,6 +2,10 @@ import PackageDescription
 
 let package = Package(
     name: "Alamofire-SwiftyJSON",
+    dependencies: [
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "4.9.0"),
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "4.0.0"),
+    ],
     products: [
         .library(
             name: "Alamofire-SwiftyJSON",
