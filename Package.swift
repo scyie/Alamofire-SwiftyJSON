c102f4ca720c2a84e15b6b7bedf3191e2131d8a2
scyie <47838491+scyie@users.noreply.github.com>
2019-10-24 14:26:09 +0900

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,20 @@
+import PackageDescription
+
+let package = Package(
+    name: "Alamofire-SwiftyJSON",
+    dependencies: [
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "4.9.0"),
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "4.0.0"),
+    ],
+    products: [
+        .library(
+            name: "Alamofire-SwiftyJSON",
+            targets: ["Alamofire-SwiftyJSON"]),
+    ],
+    targets: [
+        .target(
+            name: "Alamofire-SwiftyJSON",
+            dependencies: ["Alamofire", "SwiftyJSON"],
+            path: "Sources"),
+    ]
+)
